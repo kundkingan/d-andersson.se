d5d64248aaa661ba5cf3688522baf2c4cd875089
kundkingan <david_andersson96@outlook.com>
2018-04-01 21:43:15 +0200

    Fixed routing for server

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const api = require('./api/api.js');
-const routes = require('./routes/routes.js');
 const app = express();
 const port = process.env.PORT || '8080';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(express.static(path.join(__dirname, 'start/dist')));
-app.use(express.static(path.join(__dirname, 'projects/tracker/dist')));
+app.use('/',express.static(path.join(__dirname, 'start/dist')));
+app.use('/projects/tracker', express.static(path.join(__dirname, 'projects/tracker/dist')));
+app.use('/projects/cv', express.static(path.join(__dirname, 'projects/cv/dist')));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,11 +21,6 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'start/dist/index.html'));
-});
-
-app.use('/projects', routes);
 app.use('/api/v1', api);
 app.set('port', port);
 
