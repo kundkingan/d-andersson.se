c489e0bff9903d903077fb765e8b03c4a10bb059
kundkingan <david_andersson96@outlook.com>
2018-05-05 17:05:35 +0200

    Updated style

--- start/src/app/app.component.ts
+++ start/src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  projects = [
+  	{
+  		type: 'Web Application',
+  		name: 'Online Resume',
+  		link: 'https://cv.d-andersson.se',
+      tech: ['Angular', 'Firebase', 'SCSS']
+  	},
+  	{
+  		type: 'Progressive Web Application',
+  		name: 'Weight Tracker',
+  		link: 'https://wt.d-andersson.se',
+      tech: ['Angular', 'Firebase', 'SCSS']
+  	},
+    {
+      type: 'Progressive Web Application',
+      name: 'Crypto Tracker',
+      link: 'https://cointrack.d-andersson.se',
+      tech: ['Angular', 'Firebase', 'SCSS', 'NodeJS']
+    }
+  ]
+
+  onProject(link) {
+    window.open(link, '_blank');
+  }
 }
