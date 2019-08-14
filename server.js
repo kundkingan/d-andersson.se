77866459d6f87a625f191788422247d09d13f9c0
David Andersson <david.andersson@apsis.com>
2019-08-14 21:05:57 +0200

    minimal updates to site

--- server.js
+++ server.js
@@ -4,25 +4,27 @@ const bodyParser = require('body-parser');
 const api = require('./api/api.js');
 const app = express();
 const port = process.env.PORT || '8080';
-const fs = require('fs');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', express.static(path.join(__dirname, 'start/dist')));
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-	res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-	res.set('Access-Control-Allow-Credentials', 'true');
-	next();
+app.use((_, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,OPTIONS,POST,PUT,DELETE'
+  );
+  res.set(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.set('Access-Control-Allow-Credentials', 'true');
+  next();
 });
-
 
 app.use('/api/v1', api);
 app.set('port', port);
 
-app.listen(port)
-
-console.log('live')
+app.listen(port, () => console.log(`Listening on port ${port}.`));
--- start/src/app/app.component.ts
+++ start/src/app/app.component.ts
@@ -6,27 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
   projects = [
-  	{
-  		type: 'Web Application',
-  		name: 'Online Resume',
-  		link: 'https://cv.d-andersson.se',
+    {
+      type: 'Web Application',
+      name: 'Online Resume',
+      link: 'https://cv.d-andersson.se',
       tech: ['Angular', 'Firebase', 'SCSS']
-  	},
-  	{
-  		type: 'Progressive Web Application',
-  		name: 'Weight Tracker',
-  		link: 'https://wt.d-andersson.se',
+    },
+    {
+      type: 'Progressive Web Application',
+      name: 'Weight Tracker',
+      link: 'https://wt.d-andersson.se',
       tech: ['Angular', 'Firebase', 'SCSS']
-  	},
+    },
     {
       type: 'Progressive Web Application',
       name: 'Crypto Tracker',
       link: 'https://cointrack.d-andersson.se',
       tech: ['Angular', 'Firebase', 'SCSS', 'NodeJS']
     }
-  ]
+  ];
 
   onProject(link) {
     window.open(link, '_blank');
